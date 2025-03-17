aaed1d097501a7ec6135177fe4d31d3c8ec93ab5
AlexJuraska <151767106+AlexJuraska@users.noreply.github.com>
2025-03-17 18:35:22 +0100

    Extended scraping to include comment text and likes

--- Data gathering/postScript.js
+++ Data gathering/postScript.js
@@ -10,45 +10,43 @@ function savePostToFile() {
   hashtagElements.forEach(hashtag => {
     const text = hashtag.textContent
 
-    if (text.trim() && text.includes('#') && text.trim().replace(/#/g, '') !== "") {
+    if (text.trim() && text.includes('#') && text.trim().replace(/#/g, "") !== "") {
       uniqueTagHrefs.add(text.trim());
     }
   });
 
   // Getting comments
-  const elements = document.querySelectorAll('a.link-a11y-focus');
-  const uniqueUserHrefs = new Set();
+  const commentDivWrappers = document.querySelectorAll("div.e1970p9w2")
+  const uniqueComments = new Set();
 
-  elements.forEach(element => {
-    let href = element.getAttribute('href');
+  commentDivWrappers.forEach(div => {
+    const usernamePs = div.querySelectorAll('a.link-a11y-focus');
+    const username = usernamePs[0].getAttribute("href").substring(1);
 
-    // Filtering hrefs beginning with "/@"
-    if (href.trim() && href.trim().startsWith('/@')) {
-      href = href.trim().substring(1);
-      if (href.trim().replace(/@/g, '') !== "") {
-        uniqueUserHrefs.add(href); // Remove the '/' at the start
-      }
-    }
+    const textPs = div.querySelectorAll("p.TUXText.TUXText--tiktok-sans.css-1658qcl-StyledTUXText.e1vx58lt0");
+    const text = textPs[2].textContent;
+
+    const likesPs = div.querySelectorAll("span.TUXText.TUXText--tiktok-sans.TUXText--weight-normal");
+    const likes = likesPs[1].textContent;
+
+    uniqueComments.add(`${username}$${text}$${likes}`);
   });
 
   const filteredTagHrefs = Array.from(uniqueTagHrefs);
-  const filteredUserHrefs = Array.from(uniqueUserHrefs);
+  const filteredComments = Array.from(uniqueComments);
 
-  if (filteredTagHrefs.length > 0 || filteredUserHrefs.length > 0) {
+  if (filteredTagHrefs.length > 0 || filteredComments.length > 0) {
 
-    const hrefsToSave = filteredTagHrefs.join('\n') + '\n\n' + filteredUserHrefs.join('\n');
+    const hrefsToSave = filteredTagHrefs.join('\n') + '\n\n' + filteredComments.join('\n');
 
-    const blob = new Blob([hrefsToSave], { type: 'text/plain' });
+    const blob = new Blob([hrefsToSave], { type: "text/plain" });
 
-    // Create a link element to trigger the download
     const link = document.createElement('a');
 
-    // Link to download the file
     link.href = URL.createObjectURL(blob);
 
-    link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, '').replace(/\//g, '-') + '.txt';
+    link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, "").replace(/\//g, '-') + ".txt";
 
-    // Click the download button
     link.click();
 
     console.log("Usernames saved");
@@ -56,35 +54,3 @@ function savePostToFile() {
     console.log("Something went wrong");
   }
 }
-
-
-// javascript:(function(){
-//   function savePostToFile() {
-//     const uniqueTagHrefs = new Set();
-//     const hashtagElements = document.querySelectorAll('strong');
-//     hashtagElements.forEach(hashtag => {
-//       const text = hashtag.textContent;
-//       if (text.trim() && text.includes('#') && text.trim().replace(/#/g, '') !== "") {uniqueTagHrefs.add(text.trim());}
-//     });
-//     const elements = document.querySelectorAll('a.link-a11y-focus');
-//     const uniqueUserHrefs = new Set();
-//     elements.forEach(element => {
-//       let href = element.getAttribute('href');
-//       if (href.trim() && href.trim().startsWith('/@')) {
-//         href = href.trim().substring(1);
-//         if (href.trim().replace(/@/g, '') !== "") {uniqueUserHrefs.add(href);}
-//       }
-//     });
-//     const filteredTagHrefs = Array.from(uniqueTagHrefs);
-//     const filteredUserHrefs = Array.from(uniqueUserHrefs);
-//     if (filteredTagHrefs.length > 0 || filteredUserHrefs.length > 0) {
-//       const hrefsToSave = filteredTagHrefs.join('\n') + '\n\n' + filteredUserHrefs.join('\n');
-//       const blob = new Blob([hrefsToSave], { type: 'text/plain' });
-//       const link = document.createElement('a');
-//       link.href = URL.createObjectURL(blob);
-//       link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, '').replace(/\//g, '-') + '.txt';
-//       link.click();
-//     }
-//   }
-//   savePostToFile()
-// })();
--- Data gathering/profileVideoScript.js
+++ Data gathering/profileVideoScript.js
@@ -2,12 +2,12 @@
  * Saves the links of videos in a user page into a .txt file
  */
 function saveVideosToFile() {
-    const elements = document.querySelectorAll('a.css-1mdo0pl-AVideoContainer.e19c29qe4');
+    const elements = document.querySelectorAll("a.css-1mdo0pl-AVideoContainer.e19c29qe4");
 
     const uniqueHrefs = new Set();
 
     elements.forEach(element => {
-        const href = element.getAttribute('href');
+        const href = element.getAttribute("href");
 
         if (href && href.trim() !== "") {
             uniqueHrefs.add(href.trim());
@@ -20,17 +20,14 @@ function saveVideosToFile() {
 
         const hrefsToSave = filteredHrefs.join('\n');
 
-        const blob = new Blob([hrefsToSave], { type: 'text/plain' });
+        const blob = new Blob([hrefsToSave], { type: "text/plain" });
 
-        // Create a link element to trigger the download
         const link = document.createElement('a');
 
-        // Link to download the file
         link.href = URL.createObjectURL(blob);
 
-        link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, '').replace(/\//g, '-') + '.txt';
+        link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, "").replace(/\//g, '-') + ".txt";
 
-        // Click the download button
         link.click();
 
         console.log("Video links saved");
@@ -38,29 +35,3 @@ function saveVideosToFile() {
         console.log("Something went wrong");
     }
 }
-
-/*
-javascript:(function(){
-    function saveVideosToFile() {
-        const elements = document.querySelectorAll('a.css-1mdo0pl-AVideoContainer.e19c29qe4');
-        const uniqueHrefs = new Set();
-        elements.forEach(element => {
-        const href = element.getAttribute('href');
-        if (href && href.trim() !== "") {
-            uniqueHrefs.add(href.trim());
-        }
-        });
-        const filteredHrefs = Array.from(uniqueHrefs);
-        if (filteredHrefs.length > 0) {
-            const hrefsToSave = filteredHrefs.join('\n');
-            const blob = new Blob([hrefsToSave], { type: 'text/plain' });
-            const link = document.createElement('a');
-            link.href = URL.createObjectURL(blob);
-            link.download = window.location.href.replace(/^https:\/\/www\.tiktok\.com\//, '').replace(/\//g, '-') + '.txt';
-            link.click();
-        }
-    }
-
-    saveVideosToFile();
-})();
- */
